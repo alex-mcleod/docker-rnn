84304ce6dcac4a64fb6b8c8e5c5aa423617f074c
Alex McLeod <alex@mcleod.io>
2015-05-23 20:46:45 -0700

    Updated docker install script

--- install-docker.sh
+++ install-docker.sh
@@ -1,3 +1,6 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 sudo sh -c "echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 sudo apt-get update && sudo apt-get install -y lxc-docker
+
+cd docker-torch
+docker build --tag=tmbdev/torch-local
