4998cc3382ccc0a795449cdff349d12b9f905d63
Ubuntu <ubuntu@ip-10-35-176-244.ec2.internal>
2015-05-24 04:54:57 +0000

    Added and updated scripts

---
+++ runth.sh
@@ -0,0 +1,12 @@
+ docker run -i -t \
+ --rm \
+--device /dev/nvidia0:/dev/nvidia0 --device /dev/nvidiactl:/dev/nvidiactl --device /dev/nvidia-uvm:/dev/nvidia-uvm \
+ --dns 8.8.8.8 \
+ -v `pwd`:/work \
+ -u $(id -u) \
+ -w /work \
+ -e USER=$(whoami) \
+ -e HOME=/work \
+ -e PS1='docker-torch$ ' \
+ tmbdev/torch-local \
+ /usr/local/bin/th "$@"
