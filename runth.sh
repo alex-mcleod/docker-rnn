f0f7334b41b1abfa816ddc384676a6054c97381e
Alex McLeod <alex@mcleod.io>
2015-06-11 19:20:07 -0700

    Fixed runth.sh command

--- runth.sh
+++ runth.sh
@@ -1,6 +1,6 @@
  docker run -i -t \
  --rm \
---device /dev/nvidia0:/dev/nvidia0 --device /dev/nvidiactl:/dev/nvidiactl --device /dev/nvidia-uvm:/dev/nvidia-uvm \
+ --device /dev/nvidia0:/dev/nvidia0 --device /dev/nvidiactl:/dev/nvidiactl --device /dev/nvidia-uvm:/dev/nvidia-uvm \
  --dns 8.8.8.8 \
  -v `pwd`:/work \
  -u $(id -u) \
@@ -9,4 +9,4 @@
  -e HOME=/work \
  -e PS1='docker-torch$ ' \
  tmbdev/torch-local \
- /usr/local/bin/th "$@"
+ th "$@"
