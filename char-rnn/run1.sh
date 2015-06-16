53fe7400c3a361ab5d971f0268e2db63c45d5efe
Alex McLeod <alex@mcleod.io>
2015-06-15 23:17:48 -0700

    Updated run script

--- char-rnn/run1.sh
+++ char-rnn/run1.sh
@@ -1,5 +1,5 @@
  docker run -i -t \
- --rm \
+ --d \
  --device /dev/nvidia0:/dev/nvidia0 --device /dev/nvidiactl:/dev/nvidiactl --device /dev/nvidia-uvm:/dev/nvidia-uvm \
  --dns 8.8.8.8 \
  -v `pwd`:/work \
@@ -9,4 +9,4 @@
  -e HOME=/work \
  -e PS1='docker-torch$ ' \
  tmbdev/torch-local \
-/opt/torch/install/bin/th train.lua -data_dir data/tinyshakespeare/ -rnn_size 512 -num_layers 2 -gpuid 0 > log/train.log
+/opt/torch/install/bin/th train.lua -data_dir data/tinyshakespeare/ -rnn_size 512 -num_layers 2 -gpuid 0 > log/train
