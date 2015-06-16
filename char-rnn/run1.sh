96c22e7308c96940a320ab3aceb5144903c766fa
Alex McLeod <alex@mcleod.io>
2015-06-15 23:01:42 -0700

    Added detached option

--- char-rnn/run1.sh
+++ char-rnn/run1.sh
@@ -1,1 +1,13 @@
-../runth.sh train.lua -data_dir data/tinyshakespeare/ -rnn_size 512 -num_layers 2 -gpuid 0 > log/train.log
+ docker run -i -t \
+ --rm \
+ --device /dev/nvidia0:/dev/nvidia0 --device /dev/nvidiactl:/dev/nvidiactl --device /dev/nvidia-uvm:/dev/nvidia-uvm \
+ --dns 8.8.8.8 \
+ -d \
+ -v `pwd`:/work \
+ -u $(id -u) \
+ -w /work \
+ -e USER=$(whoami) \
+ -e HOME=/work \
+ -e PS1='docker-torch$ ' \
+ tmbdev/torch-local \
+/opt/torch/install/bin/th train.lua -data_dir data/tinyshakespeare/ -rnn_size 512 -num_layers 2 -gpuid 0 > log/train.log
